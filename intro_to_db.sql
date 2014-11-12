c1df7d5095bcc006625f60f246eea071c9991d6c
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-11-12 18:43:07 +0530

    Added relations in DB

--- intro_to_db.sql
+++ intro_to_db.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.3.0-dev
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 12, 2014 at 05:44 PM
+-- Generation Time: Nov 12, 2014 at 06:38 PM
 -- Server version: 5.5.40-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.5
 
@@ -27,12 +27,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `customers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(256) NOT NULL,
   `address` text NOT NULL,
-  `contact_no` tinytext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `contact_no` tinytext NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `address`, `contact_no`) VALUES
+(2, 'sdafhg', 'hgkl;75689', '56789');
 
 -- --------------------------------------------------------
 
@@ -41,26 +47,22 @@ CREATE TABLE IF NOT EXISTS `customers` (
 --
 
 CREATE TABLE IF NOT EXISTS `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` varchar(256) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `description` text,
   `stock` decimal(10,3) NOT NULL DEFAULT '0.000',
-  `unit` varchar(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+  `unit` varchar(5) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `items`
 --
 
 INSERT INTO `items` (`id`, `name`, `price`, `description`, `stock`, `unit`) VALUES
-(1, 'Pears Soap', '25.00', 'Pears Soap 250g', '0.000', 'pc'),
 (2, 'Pears Soap', '25.00', 'Pears Soap 250g', '0.000', 'pc'),
 (3, 'Pears Soap', '25.00', 'Pears Soap 250g', '0.000', 'pc'),
 (4, 'Pears Soap', '25.00', 'Pears Soap 250g', '0.000', 'pc'),
-(5, 'Pears Soap', '25.00', 'Pears Soap 250g', '0.000', 'pc'),
-(6, 'Lux', '10.00', 'Lux 50g Soap', '0.000', 'pc'),
 (7, 'Lays', '10.00', 'Lays chips medium sized', '0.000', 'pc'),
 (8, 'Pepsi', '12.00', 'Pepsi 300ml', '0.000', 'pc'),
 (9, 'Panteen Shampoo pouch', '3.00', 'Panteen Shampoo pouch 8ml', '0.000', 'pc'),
@@ -71,14 +73,11 @@ INSERT INTO `items` (`id`, `name`, `price`, `description`, `stock`, `unit`) VALU
 (14, 'Coke 500ml', '24.00', '', '0.000', 'pc'),
 (15, '7up 500ml', '25.00', '', '0.000', 'pc'),
 (16, 'Coke 500ml', '24.00', '', '0.000', 'pc'),
-(17, '7up 500ml', '25.00', '', '0.000', 'pc'),
 (18, 'Mirinda 500ml', '24.00', NULL, '0.000', 'pc'),
 (19, 'Dew 500ml', '25.00', NULL, '0.000', 'pc'),
 (20, 'britannia Cake', '30.00', 'britannia Cake 200g 30/-', '0.000', 'pc'),
 (21, 'britannia Cake', '30.00', 'britannia Cake 200g 30/-', '0.000', 'pc'),
-(22, 'Appy fizz 250ml', '15.00', '', '0.000', 'pc'),
-(23, 'Appy fizz 250ml', '15.00', '', '0.000', 'pc'),
-(24, 'Appy fizz 250ml', '15.00', '', '0.000', 'pc');
+(23, 'Appy fizz 250ml', '15.00', '', '0.000', 'pc');
 
 -- --------------------------------------------------------
 
@@ -87,11 +86,10 @@ INSERT INTO `items` (`id`, `name`, `price`, `description`, `stock`, `unit`) VALU
 --
 
 CREATE TABLE IF NOT EXISTS `payments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `purchase_id` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -100,13 +98,12 @@ CREATE TABLE IF NOT EXISTS `payments` (
 --
 
 CREATE TABLE IF NOT EXISTS `purchase` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `taxrate` int(11) NOT NULL DEFAULT '0',
   `discount` int(11) NOT NULL DEFAULT '0',
-  `wholesaler_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `wholesaler_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -128,13 +125,12 @@ CREATE TABLE IF NOT EXISTS `purchase_entity` (
 --
 
 CREATE TABLE IF NOT EXISTS `sale` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `taxrate` int(11) NOT NULL DEFAULT '0',
-  `discount` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `discount` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -155,12 +151,11 @@ CREATE TABLE IF NOT EXISTS `sale_entity` (
 --
 
 CREATE TABLE IF NOT EXISTS `wholesalers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `name` tinytext NOT NULL,
   `address` text,
-  `contact_no` tinytext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `contact_no` tinytext
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `wholesalers`
@@ -168,9 +163,130 @@ CREATE TABLE IF NOT EXISTS `wholesalers` (
 
 INSERT INTO `wholesalers` (`id`, `name`, `address`, `contact_no`) VALUES
 (1, 'Abc', '0', ''),
-(2, 'Abc', '0', ''),
 (3, 'Freeto lay', '0', '1209120912'),
-(4, 'Kartavya and Co.', 'E 226 Baker''s Street', '007007007007');
+(4, 'Kartavya and Co.', 'E 226 Baker''s Street', '007007007007'),
+(5, 'Me', 'sadjgjhsga', '9536232454');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+ ADD PRIMARY KEY (`id`), ADD KEY `purchase_id` (`purchase_id`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+ ADD PRIMARY KEY (`id`), ADD KEY `wholesaler_id` (`wholesaler_id`);
+
+--
+-- Indexes for table `purchase_entity`
+--
+ALTER TABLE `purchase_entity`
+ ADD KEY `purchase_id` (`purchase_id`);
+
+--
+-- Indexes for table `sale`
+--
+ALTER TABLE `sale`
+ ADD PRIMARY KEY (`id`), ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `sale_entity`
+--
+ALTER TABLE `sale_entity`
+ ADD KEY `sale_id` (`sale_id`), ADD KEY `item_id` (`item_id`);
+
+--
+-- Indexes for table `wholesalers`
+--
+ALTER TABLE `wholesalers`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `purchase`
+--
+ALTER TABLE `purchase`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sale`
+--
+ALTER TABLE `sale`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `wholesalers`
+--
+ALTER TABLE `wholesalers`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchase`
+--
+ALTER TABLE `purchase`
+ADD CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`wholesaler_id`) REFERENCES `wholesalers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `purchase_entity`
+--
+ALTER TABLE `purchase_entity`
+ADD CONSTRAINT `purchase_entity_ibfk_1` FOREIGN KEY (`purchase_id`) REFERENCES `purchase` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sale`
+--
+ALTER TABLE `sale`
+ADD CONSTRAINT `sale_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `sale_entity`
+--
+ALTER TABLE `sale_entity`
+ADD CONSTRAINT `sale_entity_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `sale_entity_ibfk_1` FOREIGN KEY (`sale_id`) REFERENCES `sale` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
